bfbb48f8c22911523dd0598826e2f3624a91e6b0
Rishit <rishit1910kumar@gmail.com>
2024-06-20 00:07:06 +0530

    added error handling

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 "use strict";
+
 import { API_KEY } from "./config.js";
 
 const newsForm = document.querySelector(".news-form");
@@ -6,31 +7,43 @@ const newsContainer = document.querySelector(".news-container");
 
 newsForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-  // console.log(e.target[0].value);
-  const searchQuery = e.target[0].value;
-  const data = await fetchNews(searchQuery);
-  // console.log(data.articles);
-  renderNews(data.articles);
-  return;
+  const searchQuery = e.target[0].value.trim(); // Trim whitespace from the search query
+  if (searchQuery) {
+    try {
+      const data = await fetchNews(searchQuery);
+      renderNews(data.articles);
+    } catch (error) {
+      console.error("Error fetching news:", error);
+      // Optionally handle error display or message to the user
+    }
+  }
 });
 
 const fetchNews = async function (query) {
-  const request = await fetch(
-    `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`
-  );
-  // console.log(request);
-  const data = await request.json();
-  return data;
+  try {
+    const response = await fetch(
+      `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`
+    );
+    if (!response.ok) {
+      throw new Error(`Failed to fetch: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching news:", error);
+    throw error; // Re-throw the error to propagate it to the caller
+  }
 };
 
-const renderNews = function (data) {
+const renderNews = function (articles) {
   newsContainer.classList.remove("hidden");
   const newsList = newsContainer.querySelector(".news-list");
-  newsList.innerHTML = "";
+  newsList.innerHTML = ""; // Clear previous content
+
   const numberOfArticlesToBeRendered = 15;
-  data.slice(0, numberOfArticlesToBeRendered).forEach((element) => {
+  articles.slice(0, numberOfArticlesToBeRendered).forEach((article) => {
     const html = `<li class="news-item">
-    <h2><a href="${element.url}" target="_blank">${element.title}</a></h2>
+      <h2><a href="${article.url}" target="_blank">${article.title}</a></h2>
     </li>`;
     newsList.insertAdjacentHTML("beforeend", html);
   });
